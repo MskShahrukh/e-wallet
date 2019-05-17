017621af7d0da92b9f05f8b8e92c5378ef9844c9
mskshahrukh <mskshahrukh@gmail.com>
2019-05-18 03:38:39 +0500

    Initial idea

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+// import * as bootstrap from ''
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: [
+    "./app.component.scss",
+    // "./node_modules/bootstrap/dist/css/bootstrap.min.css"
+  ]
 })
 export class AppComponent {
-  title = 'e-wallet';
+  title = "e-wallet";
 }

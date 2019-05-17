5afb6113ecab4fc29a04f44b5592f3b050e56f73
mskshahrukh <mskshahrukh@gmail.com>
2019-05-18 04:41:14 +0500

    fireBase added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from "@angular/core";
-// import * as bootstrap from ''
+import { AngularFirestore } from "@angular/fire/firestore";
+import { Observable } from "rxjs";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: [
-    "./app.component.scss",
-    // "./node_modules/bootstrap/dist/css/bootstrap.min.css"
-  ]
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
+  items: Observable<any[]>;
+
+  constructor(db: AngularFirestore) {
+    this.items = db.collection("items").valueChanges();
+    console.log(this.items);
+  }
   title = "e-wallet";
 }
